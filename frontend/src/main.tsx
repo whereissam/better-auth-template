1961ff21c50d6789c0f7befb2d32480959eef024
whereissam <sam135642@gmail.com>
2026-01-07 18:05:17 +0800

    feat: upgrade react version

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { WagmiProvider } from 'wagmi';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
